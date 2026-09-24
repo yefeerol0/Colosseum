671d8c26b8e711ef55d1b742c00ec297f9bb3b89
yefeerol0 <yagizefe.gamedev@outlook.com>
2026-09-25 00:03:09 +0300

    Add CombatManager class with combat methods
    * Updated project properties to fix header directories while being included to .cpp files.
    * Added three functions (for combat actions) to CombatManager to check if the directory related problem is solved.

--- ColosseumGame/includes/CombatManager.h
+++ ColosseumGame/includes/CombatManager.h
@@ -1,1 +1,13 @@
 #pragma once
+
+class Gladiator;
+
+class CombatManager
+{
+public:
+
+	void Attack(Gladiator& Enemy, int Damage);
+	void Dodge(Gladiator& Player, Gladiator& Enemy);
+	void Heal(Gladiator& Player, int Amount);
+
+};
---
+++ ColosseumGame/src/CombatManager.cpp
@@ -0,0 +1,14 @@
+#include "CombatManager.h"
+#include "Gladiator.h"
+
+void CombatManager::Attack(Gladiator& Enemy, int Damage)
+{
+}
+
+void CombatManager::Dodge(Gladiator& Player, Gladiator& Enemy)
+{
+}
+
+void CombatManager::Heal(Gladiator& Player, int Amount)
+{
+}
